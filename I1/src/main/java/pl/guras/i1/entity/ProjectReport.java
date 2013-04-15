9f1484e0bbf2e880684d18d8bac0cf620038e27b
lsteplin <lsteplin@TZWL0820.gemalto.com>
2013-04-15 11:29:53 +0200

    Zmiany w encjach i dao + generacja raportu

--- I1/src/main/java/pl/guras/i1/entity/ProjectReport.java
+++ I1/src/main/java/pl/guras/i1/entity/ProjectReport.java
@@ -1,98 +1,107 @@
 package pl.guras.i1.entity;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * @author mgorecki
  */
 @Entity
 @Table(name = "Project_Report")
+@SuppressWarnings("serial")
 public class ProjectReport implements Serializable {
-
+	
 	@Id
+	@GeneratedValue
 	private int id;
+	
+	@Enumerated(EnumType.STRING)
 	private Color color;
+	
 	private Project project;
+	
 	@ManyToOne
+	@JoinColumn(name = "WEEKLY_REPORT_ID", referencedColumnName = "ID") 
 	private WeeklyReport weeklyReport;
+	
 	private String mainAchievements;
+	
 	private String doneLastWeek;
+	
 	private String nextSteps;
+	
 	private String edc;
+	
 	private String etc;
-
+	
 	public Color getColor() {
 		return color;
 	}
-
+	
 	public void setColor(Color color) {
 		this.color = color;
 	}
-
+	
 	public Project getProject() {
 		return project;
 	}
-
+	
 	public void setProject(Project project) {
 		this.project = project;
 	}
-
+	
 	public String getMainAchievements() {
 		return mainAchievements;
 	}
-
+	
 	public void setMainAchievements(String mainAchievements) {
 		this.mainAchievements = mainAchievements;
 	}
-
+	
 	public String getDoneLastWeek() {
 		return doneLastWeek;
 	}
-
+	
 	public void setDoneLastWeek(String doneLastWeek) {
 		this.doneLastWeek = doneLastWeek;
 	}
-
+	
 	public String getNextSteps() {
 		return nextSteps;
 	}
-
+	
 	public void setNextSteps(String nextSteps) {
 		this.nextSteps = nextSteps;
 	}
-
+	
 	public String getEdc() {
 		return edc;
 	}
-
+	
 	public void setEdc(String edc) {
 		this.edc = edc;
 	}
-
+	
 	public String getEtc() {
 		return etc;
 	}
-
+	
 	public void setEtc(String etc) {
 		this.etc = etc;
 	}
-
+	
 	public WeeklyReport getWeeklyReport() {
 		return weeklyReport;
 	}
-
+	
 	public void setWeeklyReport(WeeklyReport weeklyReport) {
 		this.weeklyReport = weeklyReport;
 	}
-
+	
 	public int getId() {
 		return id;
 	}
-
+	
 	public void setId(int id) {
 		this.id = id;
 	}
--- I1/src/main/java/pl/guras/i1/entity/WeeklyReport.java
+++ I1/src/main/java/pl/guras/i1/entity/WeeklyReport.java
@@ -2,21 +2,19 @@ package pl.guras.i1.entity;
 
 import java.io.Serializable;
 import java.util.List;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * @author mgorecki
  */
 @Entity
 @Table(name = "Weekly_Report")
+@SuppressWarnings("serial")
+@NamedQuery(name = WeeklyReport.GET_WEEKLY_REPORT_BY_WEEK_AND_YEAR, query = "SELECT wr FROM WeeklyReport AS wr WHERE wr.week = ?1 AND wr.year = ?2")
 public class WeeklyReport implements Serializable {
-
+	
+	public static final String GET_WEEKLY_REPORT_BY_WEEK_AND_YEAR = "getWeeklyReportByWeekAndYear";
+	
 	@Id
 	private Long id;
 	
@@ -25,7 +23,7 @@ public class WeeklyReport implements Serializable {
 	private String lowlights;
 	
 	@OneToOne
-	@Column(name = "USER_ID")
+	@JoinColumn(name = "USER_ID")
 	private Person user;
 	
 	@Column(name = "REPORT_WEEK")
@@ -34,17 +32,17 @@ public class WeeklyReport implements Serializable {
 	@Column(name = "REPORT_YEAR")
 	private int year;
 	
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "weeklyReport")
+	@OneToMany(mappedBy = "weeklyReport", fetch = FetchType.EAGER)
 	private List<ProjectReport> projectReports;
-
+	
 	public Long getId() {
 		return id;
 	}
-
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
 	public String getHighlights() {
 		return highlights;
 	}
@@ -52,43 +50,43 @@ public class WeeklyReport implements Serializable {
 	public void setHighlights(String highlights) {
 		this.highlights = highlights;
 	}
-
+	
 	public String getLowlights() {
 		return lowlights;
 	}
-
+	
 	public void setLowlights(String lowlights) {
 		this.lowlights = lowlights;
 	}
-
+	
 	public Person getUser() {
 		return user;
 	}
-
+	
 	public void setUser(Person user) {
 		this.user = user;
 	}
-
+	
 	public int getWeek() {
 		return week;
 	}
-
+	
 	public void setWeek(int week) {
 		this.week = week;
 	}
-
+	
 	public int getYear() {
 		return year;
 	}
-
+	
 	public void setYear(int year) {
 		this.year = year;
 	}
-
+	
 	public List<ProjectReport> getProjectReports() {
 		return projectReports;
 	}
-
+	
 	public void setProjectReports(List<ProjectReport> projectReports) {
 		this.projectReports = projectReports;
 	}
